e781a3ef05c2d7dcbb57e9391f061472aa655c61
cozarkd <cozarkd@gmail.com>
2023-11-20 23:33:46 +0100

    fixing forms

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -9,29 +9,26 @@ import { Select, SelectTrigger, SelectContent, SelectItem, SelectValue } from '@
 
 
 const ContactFormSection: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
-  const [subject, setSubject] = useState('');
+
 
   return (
     <section className="py-12 w-full bg-gray-100">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold text-center mb-6">Contáctanos</h2>
         <form action="https://api.web3forms.com/submit" method="POST" className="max-w-lg mx-auto space-y-4">
+            <input type="hidden" name="access_key" value="45ee6310-e703-43b2-9fd5-530e8134cab1"></input>
+            <input type="hidden" name="redirect" value="https://web3forms.com/success"></input>
           <div>
             <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-700">Nombre</label>
-            <Input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            <Input id="name" type="text"/>
           </div>
           <div>
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-700">Email</label>
-            <Input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <Input id="email" type="email" required />
           </div>
           <div>
-            <input type="hidden" name="access_key" value="45ee6310-e703-43b2-9fd5-530e8134cab1"></input>
-            <input type="hidden" name="redirect" value="https://web3forms.com/success"></input>
             <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-700">Asunto</label>
-            <Select value={subject} onValueChange={setSubject}>
+            <Select>
               <SelectTrigger id="subject" aria-label="Asunto">
                 <SelectValue placeholder="Seleccionar Asunto" />
               </SelectTrigger>
@@ -45,7 +42,7 @@ const ContactFormSection: React.FC = () => {
           </div>
           <div>
             <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-700">Mensaje</label>
-            <Textarea id="message" value={message} onChange={(e) => setMessage(e.target.value)} rows={4} />
+            <Textarea id="message" rows={4} />
           </div>
           <div className="text-center">
             <Button type="submit">Enviar Mensaje</Button>
